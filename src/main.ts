db3e699107aba9bf037fc3656d2f2b75693fe99c
lopzz <694293256@qq.com>
2026-03-05 15:49:53 +0800

    feat: 【vant】引入toast、dialog等样式，需要手动引入

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import 'vant/lib/index.css';
+import './vantComponentsStyle.ts';
 
 const app = createApp(App);
 
